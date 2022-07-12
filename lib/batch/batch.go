72b74ceb58184047324c5365164d3657d2a6fc38
Yahor Bagatyrevich <egorbgtrvch@gmail.com>
2022-07-12 16:43:31 +0300

    init solution

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -14,5 +15,30 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	idPasser := make(chan int64)
+	var resultMutex sync.Mutex
+	var wg sync.WaitGroup
+	wg.Add(int(n))
+
+	for i := int64(0); i < pool; i++ {
+		go func() {
+			for { // цикл бесконечнй, тк сам прервется после нужного кол-ва выполнений wg.Done()
+				currentUser := <-idPasser
+				nextUser := getOne(currentUser)
+
+				resultMutex.Lock()
+				res = append(res, nextUser)
+				resultMutex.Unlock()
+
+				wg.Done() // считает за выполнение один созданный юзер, а не законченную горутину
+			}
+		}()
+	}
+
+	for j := int64(0); j < n; j++ {
+		idPasser <- j
+	}
+
+	wg.Wait()
+	return res
 }
